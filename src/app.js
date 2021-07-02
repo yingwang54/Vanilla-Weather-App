610039f58d28a3fc355728d6972d4c63aacb35ab
yingwang54 <85709022+yingwang54@users.noreply.github.com>
2021-07-02 21:05:14 +0200

    add forecast

--- src/app.js
+++ src/app.js
@@ -69,9 +69,56 @@ cityInput.innerHTML = `${cityName}`;
     iconElement.setAttribute(
       "alt", 
         response.data.weather[0].description);
-        console.log(response.data);
+        console.log(response.data)
+       showForecast(response.data.coord);
 }
 
+function showForecast(coordinates){
+  let apiKey = "f75c6779ae980097755ff7503f54fb9c";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="Date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="52"
+        />
+        <div class="Tempforecast">
+           ${Math.round(
+            forecastDay.temp.max)}°C  ${Math.round(forecastDay.temp.min )}°C 
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function searchCity(event) {
   event.preventDefault();
